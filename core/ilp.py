512c30c5f4252d905094d45a88ab8f799dd1e085
Philip Schill <philip.schill@gmx.de>
2014-08-26 11:18:59 +0200

    Changed most attributes of ILP to private, made them accessible via property getters.

--- core/ilp.py
+++ core/ilp.py
@@ -27,16 +27,6 @@ class ILP:
         block_number = str(block_number).zfill(4)
         return "PixelClassification/LabelSets/labels" + lane_number + "/block" + block_number
 
-    # Return the number of label blocks for the given lane.
-    @staticmethod
-    def number_label_blocks(project_name, lane_number):
-        lane_number = str(lane_number).zfill(3)
-        import h5py
-        proj = h5py.File(project_name, "r")
-        block_count = len(proj['PixelClassification']['LabelSets']['labels' + lane_number].keys())
-        proj.close()
-        return block_count
-
     @staticmethod
     def xyzc_axistags():
         return """{
@@ -72,29 +62,77 @@ class ILP:
 
     def __init__(self, ilastik_cmd, project_name, lane_number=0):
         self.ilastik_cmd = ilastik_cmd
-        self.project_name = project_name
-        self.lane_number = lane_number
+        self._project_name = project_name
+        self._lane_number = lane_number
 
         # Read data from project file.
-        raw_path = vigra.readHDF5(self.project_name, ILP.filepath(self.lane_number))
+        raw_path = vigra.readHDF5(project_name, ILP.filepath(lane_number))
         raw_key = os.path.basename(raw_path)
-        project_dir = os.path.dirname(os.path.realpath(self.project_name))
+        project_dir = os.path.dirname(os.path.realpath(project_name))
         raw_path = os.path.join(project_dir, raw_path[:-len(raw_key)-1])
-        self.raw_path = raw_path
-        self.raw_key = raw_key
-        self.raw_axisorder = vigra.readHDF5(self.project_name, ILP.axisorder(self.lane_number))
-        self.number_label_blocks = ILP.number_label_blocks(self.project_name, self.lane_number)
+        self._raw_path = raw_path
+        self._raw_key = raw_key
+        self._raw_axisorder = vigra.readHDF5(project_name, ILP.axisorder(lane_number))
+
+        # Get the number of label blocks.
+        s_lane_number = str(lane_number).zfill(3)
+        from h5py import File
+        proj = File(project_name, "r")
+        block_count = len(proj['PixelClassification']['LabelSets']['labels' + s_lane_number].keys())
+        proj.close()
+        self._number_label_blocks = block_count
 
         # Read number of channels from raw data.
-        self.number_of_channels = 1
-        self.number_of_probability_channels = 0
-        if self.raw_axisorder != "xyz":
-            raw = vigra.readHDF5(self.raw_path, self.raw_key)
-            self.number_of_channels = raw.shape[-1]
+        self._number_channels = 1
+        self._number_probability_channels = 0
+        if self._raw_axisorder != "xyz":
+            raw = vigra.readHDF5(raw_path, raw_key)
+            self._number_of_channels = raw.shape[-1]
+
+    # Getter for project name.
+    @property
+    def project_name(self):
+        return self._project_name
+
+    # Getter for lane number.
+    @property
+    def lane_number(self):
+        return self._lane_number
+
+    # Getter for raw path.
+    @property
+    def raw_path(self):
+        return self._raw_path
+
+    # Getter for raw key.
+    @property
+    def raw_key(self):
+        return self._raw_key
+
+    # Getter for raw axisorder.
+    @property
+    def raw_axisorder(self):
+        return self._raw_axisorder
+
+    # Getter for number label blocks.
+    @property
+    def number_label_blocks(self):
+        return self._number_label_blocks
+
+    # Getter for number channels.
+    @property
+    def number_channels(self):
+        return self._number_channels
+
+    # Getter for number probability channels.
+    @property
+    def number_probability_channels(self):
+        return self._number_probability_channels
 
     # The string raw_path/raw_key is used in ilp files as path to the raw data.
+    @property
     def raw_path_key(self):
-        return self.raw_path + "/" + self.raw_key
+        return self._raw_path + "/" + self._raw_key
 
     # Copy the raw data and reshape it to a multichannel dataset.
     def copy_raw_data_multichannel(self, file_suffix="_copy"):
@@ -109,13 +147,13 @@ class ILP:
             vigra.writeHDF5(ILP.xyzc_axistags(), self.project_name, ILP.axistags(self.lane_number))
 
         # Copy the data.
-        self.raw_path = self.raw_path[:-3] + file_suffix + ".h5"
+        self._raw_path = self.raw_path[:-3] + file_suffix + ".h5"
         if os.path.isfile(self.raw_path):
             os.remove(self.raw_path)
         vigra.writeHDF5(raw, self.raw_path, self.raw_key, compression="lzf")
 
         # Update the project file.
-        vigra.writeHDF5(self.raw_path_key(), self.project_name, ILP.filepath(self.lane_number))
+        vigra.writeHDF5(self.raw_path_key, self.project_name, ILP.filepath(self.lane_number))
 
     # Retrain the project using ilastik.
     def run_ilastik(self, probs_filename, delete_batch=False):
@@ -123,14 +161,14 @@ class ILP:
         if os.path.isfile(probs_filename):
             os.remove(probs_filename)
         cmd = '{} --headless --project {} --output_format hdf5 --output_filename_format {} {} --retrain'\
-            .format(self.ilastik_cmd, self.project_name, probs_filename, self.raw_path_key())
+            .format(self.ilastik_cmd, self.project_name, probs_filename, self.raw_path_key)
         print cmd
         os.system(cmd)
 
         # Remove batch entries from project file.
         if delete_batch:
-            import h5py
-            proj = h5py.File(self.project_name, "r+")
+            from h5py import File
+            proj = File(self.project_name, "r+")
             del proj['Batch Inputs']
             del proj['Batch Prediction Output Locations']
             proj.close()
@@ -148,15 +186,15 @@ class ILP:
         # Read raw and probability data.
         raw = vigra.readHDF5(self.raw_path, self.raw_key)
         probs = vigra.readHDF5(probs_filename, ILP.export_key)
-        self.number_of_probability_channels = probs.shape[-1]
+        self._number_probability_channels = probs.shape[-1]
 
         # Merge raw and probabilitiy data into one array.
         raw_probs_shape = list(raw.shape)
-        raw_probs_shape[-1] = self.number_of_channels + self.number_of_probability_channels
+        raw_probs_shape[-1] = self.number_channels + self.number_probability_channels
         raw_probs_shape = tuple(raw_probs_shape)
         raw_probs = numpy.zeros(raw_probs_shape)
-        raw_probs[:, :, :, :self.number_of_channels] = raw[:, :, :, :self.number_of_channels]
-        raw_probs[:, :, :, self.number_of_channels:] = probs
+        raw_probs[:, :, :, :self.number_channels] = raw[:, :, :, :self.number_channels]
+        raw_probs[:, :, :, self.number_channels:] = probs
 
         # Save the result.
         vigra.writeHDF5(raw_probs, self.raw_path, self.raw_key, compression="lzf")
