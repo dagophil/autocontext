9ca0986722545812850d310b137ca2b88f759a34
Philip Schill <philip.schill@gmx.de>
2015-07-30 12:04:20 +0200

    Added command line arguments for batch prediction and restructured the main script.

--- autocontext.py
+++ autocontext.py
@@ -6,14 +6,16 @@ How to use:
 * Select some features.
 * Add some labels.
 * Save project and exit ilastik.
-* Run this script (parameters: see __main__ part below) or use the autocontext function.
+* Run this script (parameters: see command line arguments from argparse) or use the autocontext function.
 """
-from core.ilp import ILP
-from core.labels import scatter_labels
-import colorama as col
 import sys
 import argparse
 import os
+import shutil
+import random
+import colorama as col
+from core.ilp import ILP
+from core.labels import scatter_labels
 
 
 def autocontext(ilastik_cmd, project, runs, label_data_nr, weights=None, predict_file=False):
@@ -92,67 +94,26 @@ def autocontext(ilastik_cmd, project, runs, label_data_nr, weights=None, predict
         project.replace_labels(k, blocks, block_slices)
 
 
-def process_command_line():
-    """Parse command line arguments.
+def batch_predict(args, ilastik_args):
+    """Do the batch prediction.
+
+    :param args: command line arguments
+    :param ilastik_args: additional ilastik arguments
     """
-    # Add the command line arguments.
-    parser = argparse.ArgumentParser(description="ilastik autocontext",
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("infile", type=str,
-                        help="input file")
-    parser.add_argument("-o", "--outfile", type=str, default="",
-                        help="output file")
-    parser.add_argument("-n", "--nloops", type=int, default=3,
-                        help="number of autocontext loop iterations")
-    parser.add_argument("-d", "--labeldataset", type=int, default=-1,
-                        help="id of dataset in the ilp file that contains the labels (-1: use all datasets)")
-    parser.add_argument("-c", "--cache", type=str, default="cache",
-                        help="name of the cache folder")
-    parser.add_argument("--ilastik", type=str, required=True,
-                        help="path to the file run_ilastik.sh")
-    parser.add_argument("--seed", type=int, default=None,
-                        help="the random seed")
-    parser.add_argument("--predict_file", action="store_true",
-                        help="add this flag if ilastik supports the --predict_file option")
-    parser.add_argument("--compression", default="lzf", type=str, choices=["lzf", "gzip", "szip", "None"],
-                        help="compression filter for the hdf5 files")
-    args = parser.parse_args()
-
-    # Check arguments for validity.
-    if not os.path.isfile(args.infile):
-        raise Exception("%s is not a file" % args.infile)
-    if len(args.outfile) == 0:
-        file_path, file_ext = os.path.splitext(args.infile)
-        args.outfile = file_path + "_out" + file_ext
-    if not os.path.isfile(args.ilastik) or not os.access(args.ilastik, os.X_OK):
-        raise Exception("%s is not an executable file" % args.ilastik)
-    if args.labeldataset < -1:
-        raise Exception("Wrong id of label dataset: %d" % args.d)
-    if args.compression == "None":
-        args.compression = None
-
-    return args
+    print "doing batch prediction"
+    raise NotImplementedError
 
 
-def main():
+def train(args):
+    """Do the autocontext training.
+
+    :param args: command line arguments
     """
-    """
-    import shutil
-    import random
-    import os
-
-    # Read command line arguments.
-    args = process_command_line()
-
-    # Initialize colorama and random seeds.
-    random.seed(args.seed)
-    col.init()
-
     # Copy the project file.
     # TODO: If the file exists, ask the user if it shall be deleted.
     if os.path.isfile(args.outfile):
         os.remove(args.outfile)
-    shutil.copyfile(args.infile, args.outfile)
+    shutil.copyfile(args.train, args.outfile)
 
     # Clear the cache folder.
     if os.path.isdir(args.cache):
@@ -177,6 +138,107 @@ def main():
 
     # Do the autocontext loop.
     autocontext(args.ilastik, proj, args.nloops, args.labeldataset, predict_file=args.predict_file)
+
+
+def process_command_line():
+    """Parse command line arguments.
+    """
+    # Add the command line arguments.
+    parser = argparse.ArgumentParser(description="ilastik autocontext",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    # Ilastik arguments.
+    parser.add_argument("--ilastik", type=str, required=True,
+                        help="path to the file run_ilastik.sh")
+
+    # Training arguments.
+    parser.add_argument("--train", type=str,
+                        help="path to the ilastik project that will be used for training")
+    parser.add_argument("-o", "--outfile", type=str, default="",
+                        help="output file")
+    parser.add_argument("-n", "--nloops", type=int, default=3,
+                        help="number of autocontext loop iterations")
+    parser.add_argument("-d", "--labeldataset", type=int, default=-1,
+                        help="id of dataset in the ilp file that contains the labels (-1: use all datasets)")
+    parser.add_argument("-c", "--cache", type=str, default="cache",
+                        help="name of the cache folder")
+    parser.add_argument("--seed", type=int, default=None,
+                        help="the random seed")
+    parser.add_argument("--predict_file", action="store_true",
+                        help="add this flag if ilastik supports the --predict_file option")
+    parser.add_argument("--compression", default="lzf", type=str, choices=["lzf", "gzip", "szip", "None"],
+                        help="compression filter for the hdf5 files")
+
+    # Batch prediction arguments.
+    parser.add_argument("--batch_predict", type=str,
+                        help="path of the cache folder of a previously trained autocontext that will be used for batch "
+                             "prediction")
+    parser.add_argument("--files", type=str, nargs="+",
+                        help="the files for the batch prediction")
+
+    # Do the parsing.
+    args, ilastik_args = parser.parse_known_args()
+
+    # Check if ilastik is an executable.
+    if not os.path.isfile(args.ilastik) or not os.access(args.ilastik, os.X_OK):
+        raise Exception("%s is not an executable file." % args.ilastik)
+
+    # Check for conflicts between training and batch prediction arguments.
+    if args.train is None and args.batch_predict is None:
+        raise Exception("One of the arguments --train or --batch_predict must be given.")
+    if args.train is not None and args.batch_predict is not None:
+        raise Exception("--train and --batch_predict must not be combined.")
+
+    # Check if the training arguments are valid.
+    if args.train:
+        if len(ilastik_args) > 0:
+            raise Exception("The training does not accept unknown arguments: %s" % ilastik_args)
+        if args.files is not None:
+            raise Exception("--train cannot be used for batch prediction.")
+        if not os.path.isfile(args.train):
+            raise Exception("%s is not a file." % args.train)
+        if len(args.outfile) == 0:
+            file_path, file_ext = os.path.splitext(args.train)
+            args.outfile = file_path + "_out" + file_ext
+        if args.labeldataset < -1:
+            raise Exception("Wrong id of label dataset: %d" % args.d)
+        if args.compression == "None":
+            args.compression = None
+
+    # Check if the batch prediction arguments are valid.
+    if args.batch_predict:
+        if args.files is None:
+            raise Exception("Tried to use batch prediction without --files.")
+        if not os.path.isdir(args.batch_predict):
+            raise Exception("%s is not a directory." % args.batch_predict)
+
+        # Remove the --headless and --project arguments.
+        ilastik_parser = argparse.ArgumentParser()
+        ilastik_parser.add_argument("--headless", action="store_true")
+        ilastik_parser.add_argument("--project", type=str)
+        ilastik_args = ilastik_parser.parse_known_args(ilastik_args)[1]
+
+    return args, ilastik_args
+
+
+def main():
+    """
+    """
+
+    # Read command line arguments.
+    args, ilastik_args = process_command_line()
+
+    # Initialize colorama and random seeds.
+    random.seed(args.seed)
+    col.init()
+
+    if args.train:
+        # Do the autocontext training.
+        train(args)
+    else:
+        # Do the batch prediction.
+        assert args.batch_predict
+        batch_predict(args, ilastik_args)
 
     return 0
 
